e5db0c707d6536dccbce8642c4d2ee443814e8fe
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2023-12-04 20:08:36 +0200

    components skeleton added

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,14 @@
+import Container from 'components/container/Container';
+import Header from 'components/header/Header';
+import Board from 'components/board/Board';
+
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <Container>
+      <Header></Header>
+      <Board></Board>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,7 @@
+import css from './header.module.scss';
+
+const Header = () => {
+  return <header className={css.header}>header</header>;
+};
+
+export default Header;
